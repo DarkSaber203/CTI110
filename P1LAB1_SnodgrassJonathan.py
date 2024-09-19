844a904cb313147f75f90d2b5a6f0900cf2986c0
Jonathan Snodgrass <51129540+DarkSaber203@users.noreply.github.com>
2024-09-19 10:14:25 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_SnodgrassJonathan.py
@@ -0,0 +1,11 @@
+ # Jonathan Snodgrass
+ # 9/18/24
+ # P1LAB1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+
+
+firstname = input("What is your first name: ")
+lastname = input("What is your last name: ")
+print("Hello," , firstname , lastname + "!" , "Welcome to CTI-110") 
